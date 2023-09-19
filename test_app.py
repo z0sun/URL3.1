4707109f1a8c16e164be4b2947b654978d8816b4
ty sand <ty@gmail.com>
2023-09-19 15:27:38 -0400

    Version 1 of application

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
-from application import app
+from application import app, greet
 
-
-def test_home_page():
-    response = app.test_client().get('/')
-    assert response.status_code == 200
+def test_quick():
+  a = "jeff"
+  greeting = greet(a)
+  assert greeting == "Hi jeff "
